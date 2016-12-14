b99fa9226df04ea79bd16f5dce2addc727a5582c
Anh Le <lequocanh88@gmail.com>
2016-12-14 18:19:10 +0700

    edit code to deploy app on Heroku

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express = require('express');
 
 //Create our app
 var app = express();
-app.use(express.static('public'));
-app.listen(3000, function() {
-	console.log('Express server is up on port 3000');
+const PORT = process.env.PORT || 3000; // heroku will pass the port value to this variable
+
+//open weather app dung http, khong dung https
+app.use(function (req, res, next){
+	if (req.headers('x-forwarded-proto') === 'http'){
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
 });
+
+app.use(express.static('public'));
+app.listen(PORT, function() {
+	console.log('Express server is up on port' + PORT);
+});
+
+//const gion var, ngoai tru const khong the thay doi gia tri
