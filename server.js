a0a5091e3f8baf42a49ae277e70cfba3408caad0
Anh Le <lequocanh88@gmail.com>
2016-12-15 23:13:38 +0700

    fix local host

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000; // heroku will pass the port value to thi
 
 //open weather app dung http, khong dung https
 app.use(function (req, res, next){
-	if (req.headers['x-forwarded-proto'] === 'http'){
-		next();
-	} else {
+	if (req.headers['x-forwarded-proto'] === 'https'){
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
 });
 
